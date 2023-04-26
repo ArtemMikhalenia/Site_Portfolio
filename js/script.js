87819831518ba47fa8e6249b595c692e7c0e2f73
Art <106353035+ArtemMikhalenia@users.noreply.github.com>
2023-04-26 22:42:44 +0300

    adjustments

--- js/script.js
+++ js/script.js
@@ -21,44 +21,6 @@ if (headerLink) {
    });
 }
 
-//Скрипт для анимации сайта
-const animItems = document.querySelectorAll('._anim-items');
-
-if (animItems.length > 0) {
-   window.addEventListener('scroll', animOnScroll);
-   function animOnScroll() {
-      for (let index = 0; index < animItems.length; index++) {
-         const animItem = animItems[index];
-         const animItemHeight = animItem.offsetHeight;
-         const animItemOffset = offset(animItem).top;
-         const animStart = 4;
-
-         let animItemPoint = window.innerHeight - animItemHeight / animStart;
-         if (animItemHeight > window.innerHeight) {
-            animItemPoint = window.innerHeight - window.innerHeight / animStart;
-         }
-
-         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-            animItem.classList.add('active');
-         } else {
-            if (!animItem.classList.contains('_no-anim')) {
-               animItem.classList.remove('active');
-            }
-         }
-      }
-   }
-   //Функция позиции объекта относительно верха (кроссбраузерная)
-   function offset(el) {
-      const rect = el.getBoundingClientRect(),
-         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-   }
-   setTimeout(() => {
-      animOnScroll();
-   }, 100);
-}
-
 //скролл вверх при клике на кнопку
 
 const topButton = document.querySelector('.topbutton');
@@ -195,3 +157,14 @@ document.addEventListener('keydown', function (e) {
          Element.prototype.msMatchesSelector;
    }
 })();
+
+//WOW
+
+const wow = new WOW({
+   boxClass: 'wow',
+   animateClass: 'animate__animated',
+   offset: 50,
+   mobile: true,
+   live: true,
+})
+wow.init();
